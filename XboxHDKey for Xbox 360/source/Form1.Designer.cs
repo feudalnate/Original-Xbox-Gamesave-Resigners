531675b54a0cd61c611500e5ee244782cc4e134f
feudalnate <12105780+feudalnate@users.noreply.github.com>
2022-02-20 13:46:25 -0600

    Add files via upload

---
+++ XboxHDKey for Xbox 360/source/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace XboxHDKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(22, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Console Serial Number (12 digit number)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 38);
+            this.textBox1.MaxLength = 12;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(22, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Motherboard Serial Number (16 character hexadecimal string)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 97);
+            this.textBox2.MaxLength = 16;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(24, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "XboxHDKey";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(342, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(223, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Invalid (!)";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(287, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Invalid (!)";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 213);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XboxHDKey for Xbox 360";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ XboxHDKey for Xbox 360/source/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XboxHDKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.TextChanged += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text) && textBox1.Text.Length == 12)
+                {
+                    if (ValidNumericString(textBox1.Text))
+                    {
+                        textBox3.Text = MakeXboxHDKey(textBox1.Text, textBox2.Text);
+                        textBox3.Update();
+                        label4.Visible = false;
+                        label4.Update();
+                    }
+                    else
+                    {
+                        label4.Visible = true;
+                        label4.Update();
+                    }
+                }
+                else
+                {
+                    label4.Visible = false;
+                    label4.Update();
+                }
+            };
+
+            textBox2.TextChanged += (s, e) =>
+            {
+                if (!string.IsNullOrEmpty(textBox2.Text) && textBox2.Text.Length == 16)
+                {
+                    if (ValidHexadecimalString(textBox2.Text))
+                    {
+                        textBox3.Text = MakeXboxHDKey(textBox1.Text, textBox2.Text);
+                        textBox3.Update();
+                        label5.Visible = false;
+                        label5.Update();
+                    }
+                    else
+                    {
+                        label5.Visible = true;
+                        label5.Update();
+                    }
+                }
+                else
+                {
+                    label5.Visible = false;
+                    label5.Update();
+                }
+            };
+
+        }
+
+        bool ValidNumericString(string value)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > 0)
+            {
+                char[] valid_characters = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                bool valid;
+                foreach(char character in value)
+                {
+                    valid = false;
+                    foreach (char valid_character in valid_characters)
+                    {
+                        if (character == valid_character)
+                        {
+                            valid = true;
+                            break;
+                        }
+                    }
+                    if (!valid)
+                        return false;
+
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        bool ValidHexadecimalString(string value)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > 0)
+            {
+                char[] valid_characters = new char[] {
+                    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                    'A', 'B', 'C', 'D', 'E', 'F',
+                    'a', 'b', 'c', 'd', 'e', 'f'
+                };
+                bool valid;
+                foreach (char character in value)
+                {
+                    valid = false;
+                    foreach (char valid_character in valid_characters)
+                    {
+                        if (character == valid_character)
+                        {
+                            valid = true;
+                            break;
+                        }
+                    }
+                    if (!valid)
+                        return false;
+
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        string MakeXboxHDKey(string console_serial, string motherboard_serial)
+        {
+            if (!string.IsNullOrEmpty(console_serial) && console_serial.Length == 12 && ValidNumericString(console_serial))
+            {
+                if (!string.IsNullOrEmpty(motherboard_serial) && motherboard_serial.Length == 16 && ValidHexadecimalString(motherboard_serial))
+                {
+                    byte[] XboxHDKey = new byte[0x10];
+                    byte[] console_serial_buffer = new byte[12];
+                    byte[] motherboard_serial_buffer = new byte[16 / 2];
+
+                    for(int i = 0; i < 12; i++)
+                    {
+                        console_serial_buffer[i] = Convert.ToByte(console_serial.Substring(i, 1)[0]);
+                    }
+
+                    for(int i = 0; i < 16; i += 2)
+                    {
+                        motherboard_serial_buffer[(i / 2)] = Convert.ToByte(motherboard_serial.Substring(i, 2), 16);
+                    }
+
+                    Array.Copy(motherboard_serial_buffer, 0, XboxHDKey, 4, 8);
+                    Array.Copy(console_serial_buffer, 0, XboxHDKey, 12, 4);
+
+                    return BitConverter.ToString(XboxHDKey, 0).Replace("-", "");
+                }
+            }
+
+            return "";
+        }
+
+    }
+}
